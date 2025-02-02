0b2e25a7dc421a47774ccbaa88369b65fab2e8d0
Logan Merazzi <loganmerazzi@gmail.com>
2025-02-02 10:42:18 -0300

    inclusão do app accounts

--- app/urls.py
+++ app/urls.py
@@ -19,12 +19,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import car_view, new_car_view
-
+from accounts.views import register_view, login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', car_view, name='car_list'),
     path('new_car/',new_car_view, name='new_car'),
+    path('register/',register_view, name='register'),
+    path('login/',login_view, name='login'),
 ]
 
 if settings.DEBUG:

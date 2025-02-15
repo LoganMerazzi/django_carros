62047fddedcf769c32fae87f6000d47c484cdd51
Logan Merazzi <loganmerazzi@gmail.com>
2025-02-15 18:38:19 -0300

    Alterações no formulário de novo carro para uso de Class View

--- cars/views.py
+++ cars/views.py
@@ -4,20 +4,6 @@ from cars.models import Car
 from cars.forms import CarModelForm
 # Create your views here.
 
-
-# def car_view(request):
-#     cars = Car.objects.all().order_by("-factory_year")
-#     search = request.GET.get('search')
-
-#     if search:
-#         cars = cars.filter(model__icontains = search)
-
-#     return render(
-#         template_name='cars.html', 
-#         request=request, 
-#         context={'cars': cars}
-#     )
-
 class CarsView(View):
     def get(self, request):
         cars = Car.objects.all().order_by("-factory_year")
@@ -33,14 +19,15 @@ class CarsView(View):
         )
     
 
-def new_car_view(request):
+class NewCarView(View):
 
-    if request.method == 'POST':
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+        
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('car_list')
-    else:
-        new_car_form = CarModelForm()
-
-    return render(request, 'new_car.html', context={ 'new_car_form': new_car_form})
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})

5eaea66e82feecf81eb0a00fdfb1a712450e5558
Logan Merazzi <loganmerazzi@gmail.com>
2025-02-15 17:15:32 -0300

    Adaptação da car_view para usar Class Based Views ao invés de funções no Django.

--- app/urls.py
+++ app/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import car_view, new_car_view
+from cars.views import new_car_view, CarsView #car_view, 
 from accounts.views import register_view, login_view,logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', car_view, name='car_list'),
+    path('cars/', CarsView.as_view(), name='car_list'),
     path('new_car/',new_car_view, name='new_car'),
     path('register/',register_view, name='register'),
     path('login/',login_view, name='login'),
--- cars/views.py
+++ cars/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render, redirect
+from django.views import View
 from cars.models import Car
 from cars.forms import CarModelForm
 # Create your views here.
 
 
+# def car_view(request):
+#     cars = Car.objects.all().order_by("-factory_year")
+#     search = request.GET.get('search')
 
-def car_view(request):
-    cars = Car.objects.all().order_by("-factory_year")
-    search = request.GET.get('search')
- 
-    if search:
-        cars = cars.filter(model__icontains = search)
+#     if search:
+#         cars = cars.filter(model__icontains = search)
 
-    return render(
-        template_name='cars.html', 
-        request=request, 
-        context={'cars': cars}
-    )
+#     return render(
+#         template_name='cars.html', 
+#         request=request, 
+#         context={'cars': cars}
+#     )
 
+class CarsView(View):
+    def get(self, request):
+        cars = Car.objects.all().order_by("-factory_year")
+        search = request.GET.get('search')
+    
+        if search:
+            cars = cars.filter(model__icontains = search)
+
+        return render(
+            template_name='cars.html', 
+            request=request, 
+            context={'cars': cars}
+        )
+    
 
 def new_car_view(request):
 

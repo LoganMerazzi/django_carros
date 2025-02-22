b94fc0b5a29929872e1af1589be56dac3d027c2e
Logan Merazzi <loganmerazzi@gmail.com>
2025-02-22 11:41:28 -0300

    Melhoria de uso de Generic View e criação do detalhamento do carro.

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 from cars.models import Car
 from cars.forms import CarModelForm
@@ -22,3 +22,8 @@ class NewCarCreateView(CreateView):
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
+    
+    
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'

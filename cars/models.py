a0d99851e6932932e3696a7f99c1f7587891ad00
Logan Merazzi <loganmerazzi@gmail.com>
2025-02-22 16:48:17 -0300

    Criação da Bio dos carros

--- cars/models.py
+++ cars/models.py
@@ -17,6 +17,7 @@ class Car(models.Model):
     plate = models.CharField(max_length=10, blank=True, null=True)
     price = models.DecimalField(blank=True, null=True, default=0, decimal_places=2, max_digits=8)
     car_image = models.ImageField(upload_to='cars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.model
